491d879c8094ab4863e14ae1d6a7f67aa584cd81
andrewdho-dev <andrewdho@gmail.com>
2021-11-13 23:08:40 +0900

    fix js problem

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,27 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('여보를 어떻게 불러줬으면 좋겠나요? (고양이, 냥냥이, 애기, 유선)');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = '곰탱이와 ' + myName + '의<br>행복한 크리스마스🎄';
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = '곰탱이와 ' + storedName + '의<br>행복한 크리스마스🎄';
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -23,28 +47,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-}
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-    let myName = prompt('여보를 어떻게 불러줬으면 좋겠나요? (고양이, 냥냥이, 애기, 유선)');
-    if(!myName || myName === null) {
-        setUserName();
-    } else {
-        localStorage.setItem('name', myName);
-        myHeading.innerHTML = '곰탱이와 ' + myName + '의<br>행복한 크리스마스🎄';
-    }
-}
-
-if(!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.innerHTML = '곰탱이와 ' + storedName + '의<br>행복한 크리스마스🎄';
-}
-
-myButton.onclick = function() {
-    setUserName();
 }
